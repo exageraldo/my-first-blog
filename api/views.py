95f31b54074eae38aac1400ca99fb1ebe7deaf93
Geraldo Castro <geraldo.castro@evolux.net.br>
2019-01-19 16:11:51 -0300

    Add views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User, Group
+from blog.models import Post
+from api.serializers import UserSerializer, GroupSerializer, PostSerializer
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows posts to be viewed or edited.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
