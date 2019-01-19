e6c01e7176d6e0d6ffa731517db216e70440ddc1
Geraldo Castro <geraldo.castro@evolux.net.br>
2019-01-19 16:26:40 -0300

    Add auth to api routes

--- api/views.py
+++ api/views.py
@@ -1,14 +1,19 @@
-from django.contrib.auth.models import User, Group
-from blog.models import Post
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from api.serializers import UserSerializer, GroupSerializer, PostSerializer
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
+from django.shortcuts import render
+from blog.models import Post
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
@@ -17,6 +22,9 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
@@ -25,6 +33,9 @@ class PostViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows posts to be viewed or edited.
     """
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+    
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
