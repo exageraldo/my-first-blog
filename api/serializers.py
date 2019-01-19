dc76aace665ec47848b9cdd71dcf59e3d7a482a7
Geraldo Castro <geraldo.castro@evolux.net.br>
2019-01-19 15:57:52 -0300

    Add serializers

---
+++ api/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User, Group
+from blog.models import Post
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['author', 'title', 'text', 'created_date', 'published_date']
